801ae4626b92471db7c8a7c06cf2e5aa57acc1df
Maxime Clercq <maxime.clercq@protonmail.com>
2022-03-18 08:48:22 +0100

    add huff_scrap scraper

--- lab1/crawler/crawler.py
+++ lab1/crawler/crawler.py
@@ -1,14 +1,20 @@
-from typing import Iterator, Tuple
 import scrapy
 
 
 class Crawler(scrapy.Spider):
-    name = "quotes"
-    start_urls = [
-        "https://quotes.toscrape.com/page/1/",
-        "https://quotes.toscrape.com/page/2/",
-    ]
+    name = "huff_scrap"
+    base_url = "https://www.huffpost.com/news"
 
+    # start_urls = [f"https://www.huffpost.com/news/?page={page}" for page in range(69)]
+    start_urls = ["https://www.huffpost.com/news"]
 
-def parse(self, response):
-    print(response.css("title::text").get())
+    def parse(self, response):
+
+        for title in response.css(".card__headlines"):
+            print(title)
+            yield {"title": title.css("::text").get()}
+
+        for next_page in response.css("a.pagination__next-link"):
+            # This is a recursive function that will call the `parse` function until there are no more
+            # pages to crawl.
+            yield response.follow(next_page, self.parse)
