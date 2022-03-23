159062e9be5a95d5065b9e956231bd025830dc5f
jean.demeusy <jean.demeusy@master.hes-so.ch>
2022-03-23 22:39:35 +0100

    cleaned indexer

--- lab1/indexer.py
+++ lab1/indexer.py
@@ -8,11 +8,7 @@ from queries.article import Article
 # Define a default Elasticsearch client
 connections.create_connection(hosts=["localhost"])
 
-<<<<<<< HEAD:lab1/indexer.py
 file = Path("quotes.json")
-=======
-file = Path("../crawler/quotes.json")
->>>>>>> 10bbdbc5e4201ef0d75a6abb6686a80771415316:lab1/elasticsearch/elasticsearch-huffpost-indexer.py
 
 with open(file, "r") as f:
     page_scrawler = json.load(f)
